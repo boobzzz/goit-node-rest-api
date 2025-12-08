b7ec9240e1b4a34855c6456d046e4dcdf4194ecf
Yuriy Borys <iam618384@gmail.com>
2025-12-08 12:37:10 +0200

    Setup public avatar access

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from 'node:path';
 import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
@@ -6,11 +7,11 @@ import "dotenv/config";
 import contactsRouter from "./routes/contactsRouter.js";
 import authRouter from "./routes/authRouter.js";
 import connectDatabase from "./db/connectDatabase.js";
-import {UniqueConstraintError, ValidationError} from "sequelize";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
+const __dirname = import.meta.dirname;
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -18,6 +19,8 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(notFoundHandler);
 app.use(errorHandler);
